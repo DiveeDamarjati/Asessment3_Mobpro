35feddb556de8a6106beb0076e8039c55cbd3595
Divee Ananda Damarjati <divedamarjati555@gmail.com>
2024-05-15 20:30:56 +0700

    Mengambil data dari internet

--- app/src/main/java/org/d3if0106/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0106/mobpro1/ui/screen/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if0106.mobpro1.R
 import org.d3if0106.mobpro1.ui.theme.Mobpro1Theme
 
@@ -39,11 +40,13 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier){
+    val viewModel: MainViewModel = viewModel()
     Column(
         modifier = modifier
-            .fillMaxSize().padding(16.dp)
+            .fillMaxSize()
+            .padding(16.dp)
     ){
-
+        
     }
 }
 
