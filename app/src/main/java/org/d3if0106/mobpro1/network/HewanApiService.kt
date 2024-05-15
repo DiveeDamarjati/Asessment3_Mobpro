72c13dd19761b4f26d93022dd832430d5abf0dc3
Divee Ananda Damarjati <divedamarjati555@gmail.com>
2024-05-15 21:00:42 +0700

    Menampilkan gambar dalam grid

--- app/src/main/java/org/d3if0106/mobpro1/network/HewanApiService.kt
+++ app/src/main/java/org/d3if0106/mobpro1/network/HewanApiService.kt
@@ -29,4 +29,7 @@ object HewanApi {
     val service: HewanApiService by lazy {
         retrofit.create(HewanApiService::class.java)
     }
+    fun getHewanUrl(imageId: String): String{
+        return "$BASE_URL$imageId.jpg"
+    }
 }
--- app/src/main/java/org/d3if0106/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if0106/mobpro1/ui/screen/MainViewModel.kt
@@ -2,13 +2,19 @@ package org.d3if0106.mobpro1.ui.screen
 
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.d3if0106.mobpro1.model.Hewan
 import org.d3if0106.mobpro1.network.HewanApi
 
 class MainViewModel : ViewModel() {
+
+    var data = mutableStateOf(emptyList<Hewan>())
+        private set
+
     init {
         retriveData()
     }
@@ -16,8 +22,7 @@ class MainViewModel : ViewModel() {
     private fun retriveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
