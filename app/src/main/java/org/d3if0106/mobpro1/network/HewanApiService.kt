c4095af7d5fe5e5c338ca72474a413cc83e876ef
Divee Ananda Damarjati <divedamarjati555@gmail.com>
2024-06-04 17:10:32 +0700

    Migrasi API dari static ke dynamic

--- app/src/main/java/org/d3if0106/mobpro1/network/HewanApiService.kt
+++ app/src/main/java/org/d3if0106/mobpro1/network/HewanApiService.kt
@@ -8,7 +8,8 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://raw.githubusercontent.com/" + "indraazimi/mobpro1-compose/static-api/"
+private const val BASE_URL = "https://gh.d3ifcool.org/"
+
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -21,7 +22,7 @@ private val retrofit = Retrofit.Builder()
 
 
 interface HewanApiService {
-    @GET("static-api.json")
+    @GET("hewan.php")
     suspend fun getHewan(): List<Hewan>
 }
 
@@ -30,7 +31,7 @@ object HewanApi {
         retrofit.create(HewanApiService::class.java)
     }
     fun getHewanUrl(imageId: String): String{
-        return "$BASE_URL$imageId.jpg"
+        return "${BASE_URL}image.php?id=$imageId"
     }
 }
 
